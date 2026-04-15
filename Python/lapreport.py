3d8fefbf388a02944797bc1261f55e3ad4574936
Maruf Hasan <marufhasannibir2018@gmail.com>
2026-04-15 22:59:13 +0600

    upload file

---
+++ Python/lapreport.py
@@ -0,0 +1,47 @@
+# #Variable and data types
+# x = 10
+# name = "Python"
+# print("Variable x:", x)
+# print("Datatype of name:", type(name))
+
+# # Condition (if-elif-else)
+# x = int(input("Enter a number: "))
+# if x > 10:
+#     print("Condition: x is greater than 10")
+# elif x == 10:
+#     print("Condition: x is equal to 10")
+# else:
+#     print("Condition: x is less than 10")
+
+# # For Loop
+# print("For Loop Output:")
+# for i in range(3):
+#     print(i)
+
+# # While Loop
+# print("While Loop Output:")
+# i = 0
+# while i < 3:
+#     print(i)
+#     i += 1
+    
+#  Function
+# from unicodedata import name
+
+
+# def add(a, b):
+#     return a + b
+
+# print("Function Output:", add(2, 3))
+
+# # # List
+# numbers = [1, 2, 3]
+# print("List:", numbers)
+
+# # Dictionary
+# student = {"name": "name", "age": 18}
+# print("Dictionary:", student)
+
+# # Matrix
+matrix = [[1, 2], [3, 4]]
+print("Matrix:", matrix)
